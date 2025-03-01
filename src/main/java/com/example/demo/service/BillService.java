dca3cf091bbbeadd653c5754277f3e4f71b4b635
minh <nh.minh0403@gmail.com>
2025-03-01 13:17:08 +0700

    chinh sua sidebar va them tinh nang xem hoa don cho sinh vien

--- src/main/java/com/example/demo/service/BillService.java
+++ src/main/java/com/example/demo/service/BillService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.BillFilterRequest;
 import com.example.demo.entity.Bill;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,4 +13,8 @@ public interface BillService {
     void createRoomBillForAll();
 
     public Bill createBillForRegistrations(List<Long> registrationIds);
+
+    public List<Bill> getBillsByStudentId(Long studentId);
+
+    public Page<Bill> getBillsByFilter(BillFilterRequest filterRequest);
 }
